241b270766ba1a2b0d5543c79a7f4ae0fffc1cc1
Miguel <marredondov@gmail.com>
2020-06-15 19:16:20 -0400

    first commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,50 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son requerido');
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('usuarios conectados', resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
+
+// Escuchar cuando un usuario entra o sale del chats
+socket.on('listaPersona', function(personas) {
+    console.log('Servidor:', personas);
+});
+
+//escuchar mensaje privado
+socket.on('mensajePrivado', function(personas) {
+    console.log('Servidor:', personas);
+});
